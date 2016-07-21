aa9d70eb7a6cdb102b29a79215cca025dc672dbc
NOUNI EL BACHIR <nouni.elbachir@gmail.com>
2016-07-21 12:39:09 +0100

    Initial Commit

---
+++ tests/ExempleTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elbachirnouni
+ * Date: 21/07/2016
+ * Time: 12:36
+ */
+
+namespace Test\Enimiste\Math;
+
+
+class ExempleTest extends TestCase {
+	/**
+	 * @test
+	 */
+	public function oki() {
+		$this->assertTrue( true );
+	}
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elbachirnouni
+ * Date: 21/07/2016
+ * Time: 12:34
+ */
+
+namespace Test\Enimiste\Math;
+
+
+abstract class TestCase extends \PHPUnit_Framework_TestCase {
+
+}
